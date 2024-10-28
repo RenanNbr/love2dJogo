a4a779daafefe2867cb047060521ad3609409c3f
RenanNbr <128818511+RenanNbr@users.noreply.github.com>
2024-10-28 12:40:00 -0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,33 +1,52 @@
-function  love.load()
+aviao_14bis = {
+    src = "imagens/14bis.png",
+    largura = 64,
+    altura = 64,
+    x = 0,
+    y = 0
+}
 
-    love.window.setMode(320,480,{resizable = false})
-    Background = love.graphics.newImage("imagens/background.png")
-    x,y,w,h = 20,20,60,20
-end
+function move_14bis()
 
-function love.update(dt)
-
-    if love.keyboard.isDown('up')then
-        y = y - 1
+    if love.keyboard.isDown('w') then
+        aviao_14bis.y = aviao_14bis - 1
     end
 
-    if love.keyboard.isDown('down')then
-        y = y + 1
+    if love.keyboard.isDown('s') then
+        aviao_14bis.y = aviao_14bis + 1
     end
 
-    if love.keyboard.isDown('left')then
-        x = x - 1
+    if love.keyboard.isDown('a') then
+        aviao_14bis.y = aviao_14bis - 1
     end
 
-    if love.keyboard.isDown('right')then
-        x = x + 1
+    if love.keyboard.isDown('d') then
+        aviao_14bis.y = aviao_14bis + 1
     end
     
+end
+
+function love.load()
+
+    love.window.setMode(320,480,{resizable = false})
+    love.window.setTitle('14_bis vs meteoros')
+    background = love.graphics.newImage(aviao_14bis.src)
+    aviao_14bis.imagem = love.graphics.newImage(aviao_14bis.src)
+    
+end
+
+function love.update()
+    
+   if love.keyboard.isDown('w','a','s','d') then
+    move_14bis()
+    
+   end
+
 end
 
 function love.draw()
-    
-    love.graphics.draw(Background, 0,0)
-    love.graphics.draw("fill", x,y,w,h)
 
+    love.graphics.draw(background,0,0)
+    love.graphics.draw(aviao_14bis.imagem, aviao_14bis.x, aviao_14bis.y)
+    
 end
